a797d86789434db2f5c913c22c9c60032053a231
dhanashrikandre <88744642+dhanashrikandre@users.noreply.github.com>
2024-01-22 16:55:50 +0530

    Create routing.py

---
+++ chat_project/chat_project/routing.py
@@ -0,0 +1,8 @@
+# chat_project/routing.py
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter({
+    "websocket": URLRouter(websocket_urlpatterns),
+})
